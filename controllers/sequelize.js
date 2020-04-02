c42c8bae4ee337f46feb8a1bcaeb0ab9052916e8
mayhew3 <commander.boffo@gmail.com>
2020-04-02 15:12:35 -0700

    Add node code

---
+++ controllers/sequelize.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+let config = process.env.DATABASE_URL;
+
+exports.sequelize = new Sequelize(config, {
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
---
+++ routes/routes.js
@@ -0,0 +1,41 @@
+let express = require('express');
+
+module.exports = function(app) {
+  let games = require('../controllers/games_controller');
+
+  let router = express.Router();
+
+  router.route('/games')
+    .get(games.getGames);
+
+  app.use('/api', router);
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      console.log(err.message);
+      console.log(err.stack);
+      console.log("Status: " + err.status);
+      res.status(err.status || 500).json({
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
+    console.log(err.message);
+    console.log(err.stack);
+    console.log("Status: " + err.status);
+    res.status(err.status || 500).json({
+      message: err.message,
+      error: err
+    });
+  });
+
+};
