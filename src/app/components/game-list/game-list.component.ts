e557b5efccf3692e2515b4f73111322ea6d62daa
mayhew3 <commander.boffo@gmail.com>
2020-04-08 12:00:33 -0700

    MM-1028: Sort enum

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Game} from '../../interfaces/Game';
 import fast_sort from 'fast-sort';
+import {GameSort} from './game.sort';
 
 @Component({
   selector: 'mm-game-list',
@@ -12,14 +13,14 @@ export class GameListComponent implements OnInit{
   @Input() games: Game[];
   @Input() pageSize: number;
   page = 1;
-  orderings = new Map<string, any>();
-  selectedOrdering = 'lastPlayed';
+  orderings = new Map<number, any>();
+  selectedOrdering = GameSort.ByRating;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.orderings.set('rating', game => game.personGame.rating);
-    this.orderings.set('lastPlayed', game => game.personGame.last_played);
+    this.orderings.set(GameSort.ByRating, game => game.personGame.rating);
+    this.orderings.set(GameSort.ByLastPlayed, game => game.personGame.last_played);
     this.fastSortGames();
   }
 
---
+++ src/app/components/game-list/game.sort.ts
@@ -0,0 +1,4 @@
+export enum GameSort {
+  ByRating,
+  ByLastPlayed
+}
