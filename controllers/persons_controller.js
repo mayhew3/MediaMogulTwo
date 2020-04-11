e2bed33097cb401ffcf733ad66d202506d9c0368
mayhew3 <commander.boffo@gmail.com>
2020-04-11 16:11:05 -0700

    Cleanup

--- controllers/persons_controller.js
+++ controllers/persons_controller.js
@@ -1,9 +1,4 @@
 const model = require('./model');
-const moment = require('moment');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-const _ = require('underscore');
-const ArrayService = require('./array_util');
 
 exports.getPersons = async function (request, response) {
   const persons = await model.Person.findAll();
--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -78,7 +78,6 @@ export class PersonService {
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
       // Let the app keep running by returning an empty result.
