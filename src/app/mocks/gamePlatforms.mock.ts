57d952cce36cb016fc00a679b520675452e9d4ab
mayhew3 <commander.boffo@gmail.com>
2020-04-22 21:47:44 -0700

    MM-1060: Sample data needs platforms

--- src/app/mocks/gamePlatforms.mock.ts
+++ src/app/mocks/gamePlatforms.mock.ts
@@ -1,4 +1,78 @@
 
-export const MockGamePlatforms: any[] = [
-
+export const MockGamePlatforms = [
+  {
+    "full_name": "Steam",
+    "short_name": "Steam",
+    "id": 1
+  },
+  {
+    "full_name": "Xbox",
+    "igdb_platform_id": 11,
+    "short_name": "Xbox",
+    "id": 2,
+    "igdb_name": "Xbox"
+  },
+  {
+    "full_name": "Switch",
+    "igdb_platform_id": 130,
+    "short_name": "Switch",
+    "id": 3,
+    "igdb_name": "Nintendo Switch"
+  },
+  {
+    "full_name": "Xbox One",
+    "igdb_platform_id": 49,
+    "short_name": "Xbox One",
+    "id": 4,
+    "igdb_name": "Xbox One"
+  },
+  {
+    "full_name": "Wii U",
+    "igdb_platform_id": 41,
+    "short_name": "Wii U",
+    "id": 5,
+    "igdb_name": "Wii U"
+  },
+  {
+    "full_name": "PS3",
+    "igdb_platform_id": 9,
+    "short_name": "PS3",
+    "id": 6,
+    "igdb_name": "PlayStation 3"
+  },
+  {
+    "full_name": "Xbox 360",
+    "igdb_platform_id": 12,
+    "short_name": "Xbox 360",
+    "id": 7,
+    "igdb_name": "Xbox 360"
+  },
+  {
+    "full_name": "Wii",
+    "igdb_platform_id": 5,
+    "short_name": "Wii",
+    "id": 8,
+    "igdb_name": "Wii"
+  },
+  {
+    "full_name": "NDS",
+    "igdb_platform_id": 20,
+    "short_name": "NDS",
+    "id": 9,
+    "igdb_name": "Nintendo DS"
+  },
+  {
+    "full_name": "PC",
+    "igdb_platform_id": 6,
+    "short_name": "PC",
+    "id": 10,
+    "igdb_name": "PC (Microsoft Windows)"
+  },
+  {
+    "full_name": "PS4",
+    "igdb_platform_id": 48,
+    "short_name": "PS4",
+    "id": 11,
+    "igdb_name": "PlayStation 4"
+  }
 ];
