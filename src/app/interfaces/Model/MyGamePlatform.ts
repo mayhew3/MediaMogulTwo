c5fcb625d53e56edc03bdd6652f38b0dc7bc2730
mayhew3 <commander.boffo@gmail.com>
2020-04-25 13:09:24 -0700

    MM-1065: Copy PersonGame fields to MyGameRating

--- src/app/interfaces/Model/MyGamePlatform.ts
+++ src/app/interfaces/Model/MyGamePlatform.ts
@@ -8,6 +8,16 @@ export class MyGamePlatform extends DataObject {
   person: Person;
   availableGamePlatform: AvailableGamePlatform;
 
+  platform_name = this.registerStringField("platform_name", true);
+  rating = this.registerStringField("rating", true);
+  tier = this.registerStringField("tier", true);
+  final_score = this.registerStringField("final_score", true);
+  minutes_played = this.registerStringField("minutes_played", true);
+  replay_score = this.registerStringField("replay_score", true);
+  last_played = this.registerStringField("last_played", true);
+  finished_date = this.registerStringField("finished_date", true);
+  replay_reason = this.registerStringField("replay_reason", true);
+
   getApiMethod(): string {
     return 'myPlatforms';
   }
