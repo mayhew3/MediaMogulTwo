dc982bec7aaf931f91564d91443f95a1db3b4c58
mayhew3 <commander.boffo@gmail.com>
2020-04-29 08:22:13 -0700

    MM-1072: Update sample data

--- src/app/mocks/gamePlatforms.mock.ts
+++ src/app/mocks/gamePlatforms.mock.ts
@@ -1,127 +1,99 @@
 
 export const MockGamePlatforms = [
   {
-    "full_name": "PC (Microsoft Windows)",
-    "igdb_platform_id": 6,
-    "short_name": "PC",
-    "id": 1,
-    "igdb_name": "PC (Microsoft Windows)"
-  },
-  {
-    "full_name": "PlayStation 3",
-    "igdb_platform_id": 9,
-    "short_name": "PS3",
-    "id": 2,
-    "igdb_name": "PlayStation 3"
-  },
-  {
-    "full_name": "Xbox 360",
-    "igdb_platform_id": 12,
-    "short_name": "X360",
-    "id": 3,
-    "igdb_name": "Xbox 360"
-  },
-  {
-    "full_name": "Android",
-    "igdb_platform_id": 34,
-    "short_name": "Android",
-    "id": 4,
-    "igdb_name": "Android"
-  },
-  {
-    "full_name": "iOS",
-    "igdb_platform_id": 39,
-    "short_name": "iOS",
-    "id": 5,
-    "igdb_name": "iOS"
-  },
-  {
-    "full_name": "PlayStation 4",
-    "igdb_platform_id": 48,
-    "short_name": "PS4",
-    "id": 6,
-    "igdb_name": "PlayStation 4"
-  },
-  {
-    "full_name": "Xbox One",
-    "igdb_platform_id": 49,
-    "short_name": "XONE",
-    "id": 7,
-    "igdb_name": "Xbox One"
-  },
-  {
-    "full_name": "Nintendo Switch",
-    "igdb_platform_id": 130,
-    "short_name": "Switch",
-    "id": 8,
-    "igdb_name": "Nintendo Switch"
-  },
-  {
     "full_name": "Steam",
     "short_name": "Steam",
-    "id": 9
+    "id": 1
   },
   {
     "full_name": "Xbox",
     "igdb_platform_id": 11,
     "short_name": "Xbox",
-    "id": 10,
+    "id": 2,
     "igdb_name": "Xbox"
   },
   {
     "full_name": "Switch",
     "igdb_platform_id": 130,
     "short_name": "Switch",
-    "id": 11,
+    "id": 3,
     "igdb_name": "Nintendo Switch"
+  },
+  {
+    "full_name": "Xbox One",
+    "igdb_platform_id": 49,
+    "short_name": "Xbox One",
+    "id": 4,
+    "igdb_name": "Xbox One"
   },
   {
     "full_name": "Wii U",
     "igdb_platform_id": 41,
     "short_name": "Wii U",
-    "id": 12,
+    "id": 5,
     "igdb_name": "Wii U"
   },
   {
     "full_name": "PS3",
     "igdb_platform_id": 9,
     "short_name": "PS3",
-    "id": 13,
+    "id": 6,
     "igdb_name": "PlayStation 3"
+  },
+  {
+    "full_name": "Xbox 360",
+    "igdb_platform_id": 12,
+    "short_name": "Xbox 360",
+    "id": 7,
+    "igdb_name": "Xbox 360"
   },
   {
     "full_name": "Wii",
     "igdb_platform_id": 5,
     "short_name": "Wii",
-    "id": 14,
+    "id": 8,
     "igdb_name": "Wii"
   },
   {
     "full_name": "NDS",
     "igdb_platform_id": 20,
     "short_name": "NDS",
-    "id": 15,
+    "id": 9,
     "igdb_name": "Nintendo DS"
   },
   {
     "full_name": "PC",
     "igdb_platform_id": 6,
     "short_name": "PC",
-    "id": 16,
+    "id": 10,
     "igdb_name": "PC (Microsoft Windows)"
   },
   {
     "full_name": "PS4",
     "igdb_platform_id": 48,
     "short_name": "PS4",
-    "id": 17,
+    "id": 11,
     "igdb_name": "PlayStation 4"
+  },
+  {
+    "full_name": "Android",
+    "igdb_platform_id": 34,
+    "short_name": "Android",
+    "id": 12,
+    "igdb_name": "Android"
+  },
+  {
+    "full_name": "iOS",
+    "igdb_platform_id": 39,
+    "short_name": "iOS",
+    "id": 13,
+    "igdb_name": "iOS"
   },
   {
     "full_name": "PlayStation 2",
     "igdb_platform_id": 8,
     "short_name": "PS2",
-    "id": 18,
+    "id": 14,
     "igdb_name": "PlayStation 2"
   }
 ];
