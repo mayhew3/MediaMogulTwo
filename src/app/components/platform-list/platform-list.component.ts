8adcd3d92e2b4dc68d5929134d76c0110e5a6a3a
mayhew3 <commander.boffo@gmail.com>
2020-05-02 17:57:30 -0700

    MM-1063: Show table

--- src/app/components/platform-list/platform-list.component.ts
+++ src/app/components/platform-list/platform-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PlatformService} from '../../services/platform.service';
 
 @Component({
   selector: 'mm-platform-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatformListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public platformService: PlatformService) { }
 
   ngOnInit(): void {
   }
