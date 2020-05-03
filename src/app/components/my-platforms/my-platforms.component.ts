09af9551c3b7c5d6bd612aa07e13ff5c1c996a31
mayhew3 <commander.boffo@gmail.com>
2020-05-03 12:27:05 -0700

    MM-1061: My platforms are displaying correctly using new service observable.

--- src/app/components/my-platforms/my-platforms.component.ts
+++ src/app/components/my-platforms/my-platforms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PlatformService} from '../../services/platform.service';
 
 @Component({
   selector: 'mm-my-platforms',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyPlatformsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public platformService: PlatformService) { }
 
   ngOnInit(): void {
   }
