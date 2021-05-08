9e44c64de3fa1950c56849bd1e26a5594a1c7885
Mayhew Seavey <commander.boffo@gmail.com>
2021-05-08 00:09:37 -0700

    Deprecation warning

--- src/app/services/color-transform.service.ts
+++ src/app/services/color-transform.service.ts
@@ -13,6 +13,6 @@ export class ColorTransformService {
 
     const hue = (scaledValue <= halfVal) ? scaledValue * 0.5 : (halfVal * 0.5 + (scaledValue - halfVal) * 4.5);
     const saturation = !scaledValue ? '0%' : !faded ? '50%' : '15%';
-    return `hsla(${hue}, ${saturation}, 42%, 1`;
+    return `hsla(${hue}, ${saturation}%, 42%, 1`;
   }
 }
